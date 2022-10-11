b7fb62aaac0a397bdbb882b5312944c30f61dc55
OhhAlluka <prutifex@gmail.com>
2022-10-11 14:50:20 +0100

    Added Hello World

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -18,5 +18,8 @@ public class Week1
 
         String name = InputReader.getString("Enter your name > ");
         System.out.println("Hello " + name);
+        System.out.println("Hello World");
+
+
     } 
 }
